3420dd54a450388ef4ee7c58b5eef7bbe5e98488
Leigh Williamson <leighw@us.ibm.com>
2020-12-13 17:26:35 -0600

    fix bug in app config

--- public/assistant_config.js
+++ public/assistant_config.js
@@ -1,7 +1,7 @@
 var assistant_config = {
-    "page_title":"Swiftly Virtual Assistant",
-    "integrationID":"d7f817b6-10ed-4b83-b9d2-d1a7ceed347b"
+    "page_title":"Swiftly API Tests",
+    "integrationID":"d7f817b6-10ed-4b83-b9d2-d1a7ceed347b",
     "region": "us-south",
-    "bot_titlebar":"Swiftly Virtual Assistant"
+    "bot_titlebar":"Swiftly API Tests"
 }
 export default assistant_config;
