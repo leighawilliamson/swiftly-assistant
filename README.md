d67d9d3c69a957c89c9f5563e17d1492399860b0
Leigh Williamson <leighw@us.ibm.com>
2020-12-13 14:52:53 -0600

    initial code drop

---
+++ README.md
@@ -0,0 +1,3 @@
+# Simple Web Chat Demo
+
+A mock-up application for hosting the Watson Assistant web widget on a static web page displaying a screen capture of the client's real home web page
---
+++ public/assistant_config.js
@@ -0,0 +1,7 @@
+var assistant_config = {
+    "page_title":"Swiftly Virtual Assistant",
+    "integrationID":"d7f817b6-10ed-4b83-b9d2-d1a7ceed347b"
+    "region": "us-south",
+    "bot_titlebar":"Swiftly Virtual Assistant"
+}
+export default assistant_config;
